c66f2ef646ecad1b912a4caef26e97f58033cce3
evenmn <evenmn@fys.uio.no>
2020-05-16 23:49:26 +0200

    Added source files.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vashishta-simulator',
+      version='0.1',
+      description='Package for automatized molecular dynamics simulations with the Vashishta potential',
+      url='http://github.com/evenmn/vashishta-simulator',
+      author='Even Marius Nordhagen',
+      author_email='evenmn@fys.uio.no',
+      license='MIT',
+      packages=['vashishta-simulator'],
+      zip_safe=False)
