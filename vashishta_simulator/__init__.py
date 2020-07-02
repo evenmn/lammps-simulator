0407305007816f7ca878b1b4699e5b5611151cbc
evenmn <evenmn@fys.uio.no>
2020-07-02 19:54:03 +0200

    Added more comments

--- vashishta_simulator/__init__.py
+++ vashishta_simulator/__init__.py
@@ -1,21 +1,22 @@
 import os
 from shutil import copyfile
+from re import findall
 
 class Simulator:
-    """ Initialize class
+    """Initialize class
     
-    Parameters
-    ----------
-    substance : obj
-        which potential to use
+    :param directory: working directory
+    :type directory: str
+    :param overwrite: if True, a directory with identical name will be overwritten. If False, a number extension is added to the directory name to avoid overwriting
+    :type overwrite: bool
     """
     
     from .computer import CPU
     
     def __init__(self, directory, overwrite=False):
-        self.cwd = directory
+        self.wd = directory
         if overwrite:
-            self.cwd = directory
+            self.wd = directory
             try:
                 os.makedirs(directory)
             except:
@@ -25,53 +26,60 @@ class Simulator:
             repeat=True
             while repeat:
                 try:
-                    os.makedirs(self.cwd)
+                    os.makedirs(self.wd)
                     repeat=False
                 except FileExistsError:
                     ext += 1
-                    self.cwd = directory + f"_{ext}"
-        self.cwd += "/"
+                    self.wd = directory + f"_{ext}"
+        self.wd += "/"
         
     def copy_to_wd(self, filename):
-        """Copy to working directory.
+        """Copy one or several files to working directory.
+
+        :param filename: filename or list of filenames to copy
+        :type filename: str or list of str
         """
-        head, tail = os.path.split(filename)
-        copyfile(filename, self.cwd + tail)
+
+        if type(filename) is str:
+            filename = [filename]
+        elif type(filename) is list:
+            pass
+        else:
+            raise TypeError("Argument 'filename' was to be str or list of str")
+
+        for file in filename:
+            head, tail = os.path.split(file)
+            copyfile(file, self.wd + tail)
         
     def ordered_parameter_string(self, params, param_suffices, param_list, string):
-        """ Returning an ordered list of all the parameter values.
+        """Returning an ordered list of all the parameter values
         
-        Parameters
-        ----------
-        params : dictionary
-            dictionary with all parameters.
-        param_suffices : List of str  
-            list of all parameter suffices.
-        param_list : list   
-            initial list to append parameter to.
-        string : str
-            initial string that will be extended.
+        :type params: dict
+        :param params: dictionary with all parameters
+        :type param_suffices: list of str  
+        :param param_suffices: list of all parameter suffices (e.g. "Zi") correctly ordered
+        :type param_list: list
+        :param param_list: initial list to append parameters to
+        :type string: str
+        :param string: initial string that will be extended with all parameters
         """
         for suffix in param_suffices:
             param_list.append(params[suffix])
         for param in param_list:
-            string += str(param) + 2 * " "
+            string += str(param) + "  \t"
         return string + "\n"
         
     def append_type_to_file(self, name, params, filename):
-        """ Append the actual parameter values to the parameter file.
+        """Append the actual parameter values to the parameter file.
         
-        Parameters
-        ----------
-        name : str 
-            name of interaction combo (e.g. "SiSiSi")
-        params : dictionary 
-            dictonary with all parameters
-        filename  : str
-            filename 
+        :type name: str 
+        :param name: name of interaction combo (e.g. "SiSiSi")
+        :type params: dict
+        :param params: dictionary with all parameters
+        :type filename: str
+        :param filename: parameter filename
         """
         # Split name
-        from re import findall
         prefix_list = findall('[A-Z][^A-Z]*', name)
         params_line1 = ["H", "eta", "Zi", "Zj", "r1s", "D", "r4s"] # correctly ordered
         params_line2 = ["W", "rc", "B", "xi", "r0", "C", "cos(theta)"]  # correctly ordered
@@ -85,16 +93,16 @@ class Simulator:
             
                 
     def generate_parameter_file(self, substance, filename="dest.vashishta", params={}):
-        """ Generates input parameter file for the potential. The default
+        """Generates input parameter file for the potential. The default
         parameters are the ones specified in Wang et al., so parameters
         that are not specified will fall back on these default parameters.
         
-        Parameters
-        ----------
-        filename : str   
-            filename
-        header_filename : str  
-            header file name
+        :param substance: substance to simulate
+        :type substance: str
+        :param filename: filename of parameter file
+        :type filename: str
+        :param params: dictionary of parameters that should be changed 
+        :type params: dict
         """
         # Get default parameters
         if substance == "water":
@@ -104,7 +112,7 @@ class Simulator:
             from .substance import silica
             self.params = silica
         else:
-            raise NotImplemented
+            raise NotImplementedError("The currently available substances are silica and water")
         
         # Set parameters
         for comb, parameters in params.items():
@@ -116,50 +124,36 @@ class Simulator:
         header_filename = this_dir + "/data/header.vashishta"
         self.param_file = filename
         
-        copyfile(header_filename, self.cwd + self.param_file)
+        copyfile(header_filename, self.wd + self.param_file)
         
         # Add parameters to file
         for name, params in self.params.items():
-            self.append_type_to_file(name, params, self.cwd + self.param_file)
-            
-    def set_parameter_file(self, filename, copy=True):
-        """Set parameter file that is already prepared.
-        """
-        if copy:
-            head, self.param_file = os.path.split(filename)
-            copyfile(filename, self.cwd + self.param_file)
-        else:
-            self.param_file = filename
+            self.append_type_to_file(name, params, self.wd + self.param_file)
 
     def set_lammps_script(self, filename, var={}, copy=True):
         """Set LAMMPS script
+
+        :param filename: LAMMPS input script
+        :type filename: str
+        :param var: variables to be specified by the command line
+        :type var: dict
+        :param copy: if True, the file is copied to the working directory
+        :type copy: bool
         """
         self.var = var
         if copy:
             head, self.lmp_script = os.path.split(filename)
-            copyfile(filename, self.cwd + self.lmp_script)
+            copyfile(filename, self.wd + self.lmp_script)
         else:
             self.lmp_script = filename
             
-    def run(self, computer=CPU(num_procs=4), jobscript=None):
+    def run(self, computer=CPU(num_procs=4)):
         """Run simulation
+
+        :param computer: how to run the simulation. Options are 'CPU', 'GPU', 'SlurmCPU' and 'SlurmGPU', see computer.py
+        :type computer: obj
         """
         main_path = os.getcwd()
-        os.chdir(self.cwd)
+        os.chdir(self.wd)
         computer(self.lmp_script, self.var)
         os.chdir(main_path)
-        
-        
-if __name__ == "__main__":
-
-    # EXAMPLE USAGE
-    from computer import CPU
-    
-    sim = Simulator(directory="test", overwrite=True)
-    sim.copy_to_wd("watercube_4nm.data")
-    sim.generate_parameter_file("water", filename="H2O.vashishta", params={})
-    sim.set_lammps_script("script.in", var = {}, copy=True)
-    sim.run(computer=CPU(num_procs=4), jobscript=None)
-    
-    
-    
