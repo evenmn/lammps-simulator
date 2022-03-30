2867469bf450ac9cfd4c32fc0a6bec15ea638d11
evenmn <evenmn@fys.uio.no>
2022-03-30 22:48:36 +0200

    Added version number to '__init__.py'

--- lammps_simulator/__init__.py
+++ lammps_simulator/__init__.py
@@ -1,2 +1,3 @@
+__version__ = "1.1.0"
 from .simulator import Simulator
 sim = Simulator()
--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
+import lammps_simulator
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='lammps-simulator',
-      version='1.1.0',
+      version=lammps_simulator.__version__,
       description='Python interface for running LAMMPS input scripts',
-      long_description = file: README.md,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/evenmn/lammps-simulator',
       author='Even Marius Nordhagen',
       author_email='evenmn@mn.uio.no',
