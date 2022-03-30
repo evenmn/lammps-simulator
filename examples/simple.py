7c8830d55a6e64919403fecb51fca06f515633af
evenmn <evenmn@fys.uio.no>
2022-03-30 22:03:40 +0200

    Removed redundant examples

--- examples/simple.py
+++
@@ -1,6 +0,0 @@
-from lammps_simulator import Simulator
-from lammps_simulator.computer import CPU
-
-sim = Simulator(directory="simulation")
-sim.set_input_script("script.in")
-sim.run(computer=CPU(num_procs=4, lmp_exec="lmp_mpi"))
--- examples/slurm_cpu.py
+++
@@ -1,8 +0,0 @@
-from lammps_simulator import Simulator
-from lammps_simulator.computer import SlurmCPU
-
-computer = SlurmCPU(1, lmp_exec="lmp")
-
-sim = Simulator(directory="simulation")
-sim.set_input_script("in.lammps", temp=300)
-sim.run(computer=computer)
