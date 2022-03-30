4665ef481ea4ef6055db86ad293906d91f2c3091
Even Marius Nordhagen <evenmn@fys.uio.no>
2022-03-30 22:38:58 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(name='lammps-simulator',
       version='1.1.0',
       description='Python interface for running LAMMPS input scripts',
+      long_description = file: README.md,
       url='http://github.com/evenmn/lammps-simulator',
       author='Even Marius Nordhagen',
       author_email='evenmn@mn.uio.no',
